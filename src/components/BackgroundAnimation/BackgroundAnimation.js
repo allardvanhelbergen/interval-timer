2643a6b812984604e835af2c9e83d00216512980
Allard van Helbergen <avanhelbergen@atlassian.com>
2019-07-28 19:59:10 +1000

    Add background animation. Layout is broken now.

---
+++ src/components/BackgroundAnimation/BackgroundAnimation.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from '@emotion/styled'
+import tokens from '../../utils/styleTokens';
+
+
+const particleSize = 10; // vmin
+const animationDuration = 6;
+const particleCount = 10;
+const colors = [ tokens.colorGray1, tokens.colorGray2, tokens.colorGray4 ];
+
+let randomInt = (max, min = 0) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: ${tokens.colorGray3};
+
+  & span {
+    width: ${particleSize}vmin;
+    height: ${particleSize}vmin;
+    border-radius: ${particleSize}vmin;
+    backface-visibility: hidden;
+    position: absolute;
+    animation-name: move;
+    animation-duration: ${animationDuration};
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    
+    ${() => {
+      let result = '';
+      let bgColor;
+
+      for (let i = 0; i < particleCount; i += 1) {
+        bgColor = colors[randomInt(colors.length - 1)];
+
+        result += `
+          &:nth-of-type(${i}) {
+            color: ${bgColor};
+            top: ${Math.random() * 100}%;
+            left: ${Math.random() * 100}%;
+            animation-duration: ${(Math.random() * animationDuration) + 10}s;
+            animation-delay: ${(Math.random() * (animationDuration + 5)) * -1}s;
+            transform-origin: ${(randomInt(50) - 25)}vw ${(randomInt(50) - 25)}vh;
+            box-shadow: 
+              ${particleSize * 2 * ((Math.random() > 0.5) ? 1 : -1)}vmin 
+              0 
+              ${(Math.random() + 0.5) * particleSize * 0.5}vmin 
+              ${bgColor};
+            
+            // $blurRadius: (random() + 0.5) * $particleSize * 0.5;
+            // $x: if(random() > 0.5, -1, 1);
+            // box-shadow: ($particleSize * 2 * $x) 0 $blurRadius currentColor;
+          }
+        `
+      }
+
+      return result;
+    }}
+  }
+
+  @keyframes move {
+    100% {
+      transform: translate3d(0, 0, 1px) rotate(360deg);
+    }
+  }
+`;
+
+function BackgroundAnimation(props) {
+  let particles = [];
+
+  for (let i = 0; i < particleCount; i += 1) {
+    particles.push(<span key={i}></span>);
+  }
+
+  return (
+    <Background>
+      {particles}
+    </Background>
+  );
+}
+
+export default BackgroundAnimation;
--- src/index.js
+++ src/index.js
@@ -2,26 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { injectGlobal } from 'emotion';
 import App from './components/App';
-import tokens from './utils/styleTokens';
+// import tokens from './utils/styleTokens';
+import { globalStyles } from '../src/utils/globalStyles';
 import * as serviceWorker from './serviceWorker';
 import * as intervalData from './intervals.json';
 
 injectGlobal`
-  body {
-    background-color: gray;
-    color: #333;
-    font-family: ${tokens.fontFamily2};
-    font-size: ${tokens.fontSize};
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    margin: 0;
-    padding: 0;
-  }
-
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
-      monospace;
-  }
+  ${globalStyles}
 `;
 
 ReactDOM.render(
