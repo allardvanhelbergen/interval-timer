bf7218ab2669d7d050259cd9af3ab0e50f93587d
Allard van Helbergen <avanhelbergen@atlassian.com>
2019-09-29 18:28:27 +1000

    Style countdown face.

--- src/utils/styleTokens.js
+++ src/utils/styleTokens.js
@@ -2,6 +2,7 @@ import * as tokens from '@vanhelbergen/design-tokens/build/js/variables';
 
 export default {
   ...tokens,
-  fontFamilyBold: `Impact, Haettenschweiler, "Franklin Gothic Bold", Charcoal, 
-    "Helvetica Inserat", "Bitstream Vera Sans Bold", "Arial Black", sans-serif;`,
+  fontFamilyBold: `'Changa One', sans-serif`, 
+  grid: 8, // px
+  BorderRadiusDefault: .2, // rem
 };
